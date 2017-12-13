c44bfb37953a4735ce800387195217556f0191ae
Juancarlos Jauregui <jauregui3@gmail.com>
2017-12-12 20:34:15 -0800

    Add handleError() method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -30,6 +30,20 @@ export class HeroService {
       );
   }
 
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
   private log(message: string) {
     this.messageService.add('HeroService: ' + message);
   }
