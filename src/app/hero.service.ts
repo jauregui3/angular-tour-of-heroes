e75d4684680a674455a33516505a37b11ff80967
Juancarlos Jauregui <jauregui3@gmail.com>
2017-12-13 21:19:18 -0800

    Refactor getHero() in HeroService to use http and tap

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,8 +19,11 @@ export class HeroService {
   private messageService: MessageService) { }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   getHeroes (): Observable<Hero[]> {
